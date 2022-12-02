5a1ef4420dc0af4d70f6ae77b1fd46f14c198c59
Selena Paik <ip9911@naver.com>
2022-12-01 23:33:33 -0500

    Time: 1052 ms (48.93%), Space: 28.5 MB (89.32%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,12 +4,11 @@ class Solution:
         if(len(nums)==1): return 1
         nums.sort()
         fin=[]
-        
         ans, res=nums[0],1
         for i in range(1,len(nums)):
             if(nums[i-1]+1==nums[i]):
                 res+=1
-                ans+=1
+                #ans+=1
             elif(nums[i-1]+1<nums[i]):
                 res=1
             fin.append(res)
